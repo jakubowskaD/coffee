0ce0f3c672554a7b3bc38fd9dc8ee29d02ee25d9
jakubowskaD <jakubowska.dj@gmail.com>
2022-04-25 11:41:40 +0200

    Menu

---
+++ src/main/java/CoffeeMachine.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CoffeeMachine {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Menu:" +
+            "1. Coffee" +
+            "2. Clining" +
+            "3. Off");
+
+}
